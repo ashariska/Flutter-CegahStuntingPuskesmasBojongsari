110ec08eff82894cb0bbfd671aa2290ac62c0faa
ashariska <ashariska@users.noreply.github.com>
2025-09-07 11:58:03 +0000

    deploy: 37cda6c88cad94252e8b845c72a0609dc8761e8e

--- main.dart.js
+++ main.dart.js
@@ -119093,7 +119093,7 @@ A.akh.prototype={
 $1(a){return A.k(a.a+1)+". "+a.b},
 $S:682}
 A.VG.prototype={
-N(a){return new A.H4(A.V(["/",new A.arr(),"/home",new A.ars()],t.N,t.y_),"/","Cegah Stunting",!1,null)}}
+N(a){return new A.H4(A.V(["/",new A.arr(),"/home",new A.ars()],t.N,t.y_),"/","Cegah Stunting Puskesmas Bojongsari",!1,null)}}
 A.arr.prototype={
 $1(a){return new A.ux(null)},
 $S:683}
