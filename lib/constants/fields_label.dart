eaa21ea1bc70503151adb1147cec2d0ccce3546b
Asha Riska Amalia <ashariska24@gmail.com>
2025-09-07 19:05:14 +0700

    init github pages #3

--- lib/constants/fields_label.dart
+++ lib/constants/fields_label.dart
@@ -34,7 +34,7 @@ final Map<String, String> fieldLabels = {
 
   // hasil prediksi / rekomendasi
   "indikasi_awal":
-      "Indikasi Awal (Dasar Klinis Berdasarkan Tinggi Badan Terhadap Umur)",
+      "Indikasi Awal \n(Dasar Klinis Berdasarkan Tinggi Badan Terhadap Umur)",
   "probabilitas_stunting": "Prediksi Stunting",
   "probabilitas_normal": "Prediksi Normal",
   "indikasi": "Penyebab",
